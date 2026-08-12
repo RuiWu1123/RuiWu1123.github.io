f323d23d0d1b5d1745a5428c1f9560bea118f6fa
Rui Wu <rw761@scarletmail.rutgers.edu>
2026-08-12 15:16:11 -0400

    Swap the award news dates: the Novieli award post is the earlier of the two LinkedIn posts

--- constants.ts
+++ constants.ts
@@ -116,8 +116,8 @@ export const PUBLICATIONS: Publication[] = [
 export const NEWS_ITEMS: NewsItem[] = [
   {
     date: "2026/8/9",
-    content: "Received the Novieli Award in the 2026 Rutgers Computer Science Departmental Awards!",
-    link: "https://www.cs.rutgers.edu/news-events/highlights/highlight-item/celebrating-excellence-2026-rutgers-computer-science-department-awards"
+    content: "Named a 2026 Paul Robeson Scholar, an honor for students who complete and defend a senior thesis through original research!",
+    link: "https://www.cs.rutgers.edu/news-events/highlights/highlight-item/celebrating-excellence-2026-paul-robeson-scholars"
   },
   {
     date: "2026/7/9",
@@ -126,8 +126,8 @@ export const NEWS_ITEMS: NewsItem[] = [
   },
   {
     date: "2026/5/7",
-    content: "Named a 2026 Paul Robeson Scholar, an honor for students who complete and defend a senior thesis through original research!",
-    link: "https://www.cs.rutgers.edu/news-events/highlights/highlight-item/celebrating-excellence-2026-paul-robeson-scholars"
+    content: "Received the Novieli Award in the 2026 Rutgers Computer Science Departmental Awards!",
+    link: "https://www.cs.rutgers.edu/news-events/highlights/highlight-item/celebrating-excellence-2026-rutgers-computer-science-department-awards"
   },
   {
     date: "2026/4/6",
